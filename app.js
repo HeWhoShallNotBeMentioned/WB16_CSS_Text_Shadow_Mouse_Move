5c4c5a0295ed8e1933468152bb207e7d92206e35
Chris Underwood <cunderwoodmn@gmail.com>
2018-01-02 20:33:40 -0800

    figure out how to add the position  of the cursor when over hero and h1

--- app.js
+++ app.js
@@ -4,7 +4,18 @@ const hero = document.querySelector('.hero');
 const text = hero.querySelector('h1');
 
 function shadow (e) {
-  console.log(e);
+  const width = hero.offsetWidth;
+  const height = hero.offsetHeight;
+  //const { offsetWidth: width, offsetHeight: height } = hero; destructured version (es6) of the above 2 lines
+  let {offsetX: x, offsetY: y} = e;
+
+  //this = hero as we are listening for that. When e.target is anything but that, in this case, h1 if is envolked.
+  if (this !== e.target) {
+     x = x + e.target.offsetLeft;
+     y = y + e.target.offsetTop;
+   }
+
+   console.log(x, y);
 }
 
 hero.addEventListener('mousemove', shadow);
